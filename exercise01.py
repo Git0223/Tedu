0d66e0d6b6d839a9ecd9a838e0453273b2d77d5c
Liuyl <18329382674@163.com>
2020-12-25 12:05:22 +0800

    atash

--- exercise01.py
+++ exercise01.py
@@ -2,3 +2,5 @@ for i in range(10):
     for r in range(1,i+1):
         print("%s*%s=%s\t"%(i,r,i*r),end="")
     print()
+    print()
+    print()
--- hhh.py
+++ hhh.py
@@ -1,2 +1,4 @@
 a = [1,2,3,None,(),[],]
 print(len(a))
+print(len(a))
+print(len(a))
