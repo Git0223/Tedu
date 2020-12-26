26cbaed296eccdd76a4c3daa6cf11a2bde53afb8
Liuyl <18329382674@163.com>
2020-12-26 09:08:35 +0800

    stash

--- exercise01.py
+++ exercise01.py
@@ -2,5 +2,3 @@ for i in range(10):
     for r in range(1,i+1):
         print("%s*%s=%s\t"%(i,r,i*r),end="")
     print()
-    print()
-    print()
--- hhh.py
+++ hhh.py
@@ -1,4 +1,3 @@
 a = [1,2,3,None,(),[],]
 print(len(a))
-print(len(a))
-print(len(a))
+
